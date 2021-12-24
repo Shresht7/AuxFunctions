b038671832f23efc9d4c33ed949a613dc6175f73
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-12-25 01:37:07 +0530

    Create helper function to determine elapsed time

---
+++ src/time/time.ts
@@ -0,0 +1,17 @@
+/**
+ * Utility function to determine the elapsed time since start
+ * 
+ * usage:
+ * 
+ * ```ts
+ * const delta = time() //  Returns a closure with the start time
+ * doTimeConsumingStuff()
+ * const elapsed = delta()  //  Call the closure to calculate the elapsed time since start
+ * 
+ * console.log(elapsed + 'ms')     //  Elapsed time is in ms
+ * ```
+ * 
+ * @param start Start time (in ms) - default: Date.now()
+ * @returns Elapsed time since start (in ms)
+ */
+const time = (start: number = Date.now()) => () => (Date.now() - start).toFixed(2)

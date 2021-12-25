94864dc6640a3e0ba238173260bf7c02b7c69dcd
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-12-25 15:37:00 +0530

    Create strings/capitalize helper function

---
+++ src/strings/capitalize.ts
@@ -0,0 +1,6 @@
+/**
+ * Capitalizes the given string
+ * @param str String to capitalize
+ * @returns Capitalized string
+ */
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)

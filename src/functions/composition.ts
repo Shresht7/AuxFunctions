3f66f87dc66d0190edcae6569ec7ec264d487d35
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-12-25 15:50:33 +0530

    Create function composition helpers

---
+++ src/functions/composition.ts
@@ -0,0 +1,27 @@
+/**
+ * Composes the given function as a pipe
+ * 
+ * usage:
+ * 
+ * ```ts
+ * const header = pipe(inverse, bold, color.red)('Project Title')
+ * ```
+ * 
+ * @param fns Variadic array of callback functions to pipe
+ * @returns Piped function
+ */
+const pipe = (...fns: Function[]) => (...args: any[]) => fns.reduce((acc, currFn) => currFn(...acc), args)
+
+/**
+ * Composes the given function
+ * 
+ * usage:
+ * 
+ * ```ts
+ * const rootMeanSquare = compose(root, mean, square)(2, 5)
+ * ```
+ * 
+ * @param fns Variadic array of callback functions to compose
+ * @returns Composed function
+ */
+const compose = (...fns: Function[]) => (...args: any[]) => fns.reduceRight((acc, currFn) => currFn(...acc), args)

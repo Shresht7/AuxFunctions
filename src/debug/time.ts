4a4d4da926b00673d94b466af3e0958964d59920
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-15 13:18:38 +0530

    Create debug timer

---
+++ src/debug/time.ts
@@ -0,0 +1,11 @@
+/**
+ * Reports the execution time for the given function
+ * @param fn Function to measure execution time of
+ * @param args Arguments to pass to the function
+ */
+export function timeExecution<T extends Array<any>>(fn: (...args: T) => void, ...args: T) {
+    const label = `Time to execute ${fn.name}`
+    console.time(label)
+    fn(...args)
+    console.timeEnd(label)
+}

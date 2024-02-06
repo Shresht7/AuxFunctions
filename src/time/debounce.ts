39e6c2f0a961d3bc9cd77312cf8c9aafb8a553da
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-06 13:48:47 +0530

    Add `invoke` and `cancel` methods to the `debounce` function

--- src/time/debounce.ts
+++ src/time/debounce.ts
@@ -1,13 +1,58 @@
+// DEBOUNCE
+// --------
+
 /**
  * Creates a debounced version of a function that delays its execution until after a specified wait time.
+ * 
  * @param fn The function to debounce.
  * @param wait The number of milliseconds to wait before executing the debounced function.
  * @returns A debounced version of the original function.
+ * 
+ * @example
+ * const debounced = debounce(() => console.log('Hello, World!'), 1000);
+ * debounced(); // Does nothing immediately.
+ * debounced(); // Does nothing immediately.
+ * debounced(); // Invokes the debounced function after 1 second.
+ * 
+ * @example
+ * const debounced = debounce(() => console.log('Hello, World!'), 1000);
+ * debounced.cancel(); // Cancels the debounced function invocation.
+ * 
+ * @example
+ * const debounced = debounce(() => console.log('Hello, World!'), 1000);
+ * debounced.invoke(); // Invokes the debounced function immediately.
  */
-function debounce<F extends (...args: any[]) => any>(fn: F, wait: number) {
-    let timeoutID: NodeJS.Timeout;
-    return function (this: ThisParameterType<F>, ...args: Parameters<F>) {
-        clearTimeout(timeoutID);
-        timeoutID = setTimeout(() => fn.apply(this, args), wait);
+function debounce<F extends (...args: any[]) => any>(fn: F, wait: number): DebouncedFn<F> {
+
+    /** The identifier of the timeout used to delay the function execution. */
+    let timeout: NodeJS.Timeout;
+
+    /** The debounced function that delays its execution until after a specified wait time. */
+    const debouncedFn = function (this: ThisParameterType<F>, ...args: Parameters<F>) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => fn.apply(this, args), wait);
+    };
+
+    /** Cancels the debounced function invocation to prevents it from being called. */
+    debouncedFn.cancel = () => clearTimeout(timeout);
+
+    /** Invokes the debounced function immediately. */
+    debouncedFn.invoke = function (this: ThisParameterType<F>, ...args: Parameters<F>) {
+        clearTimeout(timeout);
+        fn.apply(this, args);
     } as F;
+
+    return debouncedFn as DebouncedFn<F>;
+
 }
+
+// TYPE DEFINITIONS
+// ----------------
+
+/** The debounced version of a function that delays its execution until after a specified wait time. */
+type DebouncedFn<F extends (...args: any[]) => any> = F & {
+    /** Cancels the debounced function invocation to prevents it from being called. */
+    cancel: () => void;
+    /** Invokes the debounced function immediately. */
+    invoke: F;
+};

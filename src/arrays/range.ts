d9690182e0dd14cd066120b7488e15ec6869ff17
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-06 14:50:34 +0530

    Refactor range function to use a generator

--- src/arrays/range.ts
+++ src/arrays/range.ts
@@ -1,11 +1,27 @@
+// -----
+// RANGE
+// -----
+
 /**
- * Generates an array with values ranging from start to end
- * @param start Start range at
- * @param end End range at
- * @param step Range step
- * @returns Array with values ranging from start to end
+ * Generates a sequence of numbers within a specified range.
+ * @param start The starting number of the range.
+ * @param end The ending number of the range (exclusive).
+ * @param step The increment value between each number in the range.
+ * @returns An iterator that yields the numbers in the range.
+ * 
+ * @example
+ * for (const i of range(0, 5)) {
+ *    console.log(i); // Logs 0, 1, 2, 3, 4
+ * }
+ * @example
+ * const numbers = [...range(0, 5)]; // [0, 1, 2, 3, 4]
+ * @example
+ * const numbers = [...range(0, 5, 2)]; // [0, 2, 4]
+ * @example
+ * const numbers = [...range(5, 0, -1)]; // [5, 4, 3, 2, 1]
  */
-export function range(start: number, end: number, step: number = 1) {
-    const len = Math.floor((end - start) / step) + 1    //  Length of range
-    return new Array(len).fill(0).map((_, i) => start + (i * step)) //  Build range array
+export function* range(start: number, end: number, step: number = 1) {
+    for (let i = start; i < end; i += step) {
+        yield i
+    }
 }
